6a8040c32783919f84a203a17984022de05ad246
General Angels <42120249+GeneralAngels@users.noreply.github.com>
2021-11-26 14:27:50 +0200

    mine do not copy frfr god on god original nft

--- Gyroscope.java
+++ Gyroscope.java
@@ -1,8 +1,25 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-/** Add your docs here. */
-public class Gyroscope {}
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+public abstract class Gyroscope {
+    private static PigeonIMU pigeon = new PigeonIMU(30);
+
+    public static double getAngle() {
+        double[] ypr = new double[3];
+        pigeon.getYawPitchRoll(ypr);
+        return ypr[0];
+    }
+
+    public static void setAngle(double angle) {
+        pigeon.setYaw(angle);
+    }
+
+    public static void reset() {
+        pigeon.setYaw(0);
+        pigeon.setFusedHeading(0);
+
+    }
+
+}
