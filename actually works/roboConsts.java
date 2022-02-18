64b3d94bd91c9760a1f006cd934fa7bb33aadc9c
General Angels <42120249+GeneralAngels@users.noreply.github.com>
2022-02-18 14:11:06 +0200

    Update roboConsts.java

--- actually works/roboConsts.java
+++ actually works/roboConsts.java
@@ -6,16 +6,16 @@ package frc.robot;
 
 /** Add your docs here. */
 public class roboConsts { // 8/62
-    public final static double RATIO_BETWEEN_WHEELS_AND_MOTOR = 62/8;
+    public final static double RATIO_BETWEEN_WHEELS_AND_MOTOR = 62/8.0;
     public final static double PULSES_PER_REVOLUTION = 2048;
     public final static double PULSES_PER_WHEEL_ROTATION = PULSES_PER_REVOLUTION * RATIO_BETWEEN_WHEELS_AND_MOTOR;
     
     public final static double WHEEL_DIAMETER = 0.1524;
-    public final static double ENCODER_TO_RADIAN = 2 * (Math.PI) / PULSES_PER_WHEEL_ROTATION;
-    public final static double ENCODER_TO_M = ENCODER_TO_RADIAN * WHEEL_DIAMETER;
-    public final static double WHEEL_SCOPE = (WHEEL_DIAMETER) * Math.PI;
+    public final static double ENCODER_TO_RADIAN = (Math.PI) / PULSES_PER_WHEEL_ROTATION;
+    public final static double ENCODER_TO_M = ENCODER_TO_RADIAN * WHEEL_DIAMETER; // 0.0004
+    public final static double WHEEL_SCOPE = (WHEEL_DIAMETER) * Math.PI; // 0.00006 // ~166666 for meter
     public final static double METER_DRIVE = (1 / WHEEL_SCOPE) * PULSES_PER_WHEEL_ROTATION;
 
-    public final static double MAX_POWER = 0.3;
+    public final static double MAX_POWER = 0.7;
     public final static double MAX_TURN_POWER = 0.15;
 }
